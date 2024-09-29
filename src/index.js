0af0ef9094901521bc848f1abc855815092fea41
codder077 <120477792+codder077@users.noreply.github.com>
2024-09-29 11:51:25 +0530

    final commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { CONNECT_DB } from "./db/mongo.connection.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+dotenv.config();
+CONNECT_DB();
+
+import bookRoute from "./routes/book.route.js";
+import transactionRoute from "./routes/transaction.route.js";
+import userRoute from "./routes/user.route.js";
+
+app.use("/api/v1/books", bookRoute);
+app.use("/api/v1/transactions", transactionRoute);
+app.use("/api/v1/users", userRoute);
+
+app.listen(process.env.PORT, (err) => {
+  if (err) throw err;
+  console.log(`server is running on port ${process.env.PORT}`);
+});
